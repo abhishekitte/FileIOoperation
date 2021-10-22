0aa34a9f65f718c56cf15caf7ecb1d2d6dba0128
ABHI <abhishekitte11@gmail.com>
2021-10-22 18:27:50 +0530

    [ABHISHEK] Added a code to read all lines in a file

--- FileIOoperation.cs/FileIO/FileReadWriteOpertion.cs
+++ FileIOoperation.cs/FileIO/FileReadWriteOpertion.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileIOoperation.FileIO
 {
@@ -11,6 +7,7 @@ namespace FileIOoperation.FileIO
     {
         public bool FilExists(string path)
         {
+            //File is a class
             if (File.Exists(path))
             {
                 Console.WriteLine("File exists");
@@ -23,6 +20,27 @@ namespace FileIOoperation.FileIO
             }
         }
 
-
+        //Method to read all lines in a file 
+        public void ReadAllLinesInFile()
+        {
+            try
+            {
+                string[] lines;
+                string path = @"C:\Users\pihu\source\repos\FileIOoperation.cs\FileIOoperation.cs\FileIO\Sample.txt";
+                //Reads all lines in file one by one 
+                lines = File.ReadAllLines(path);
+                if (lines.Length != 0)
+                {
+                    foreach (var i in lines)
+                    {
+                        Console.WriteLine(i + " ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
