1a07c94156ccdb6cdb13dd3cbdc8c72d56f9fca5
ABHI <abhishekitte11@gmail.com>
2021-10-22 18:20:56 +0530

    [ABHISHEK] Added a code to check txt file

---
+++ FileIOoperation.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileIOoperation.FileIO;
+using System;
+
+namespace FileIOoperation.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO Operations");
+            string path = @"C:\Users\pihu\source\repos\FileIOoperation.cs\FileIOoperation.cs\FileIO\Sample.txt";
+            FileReadWriteOperation file = new FileReadWriteOperation();
+            file.FilExists(path);
+            Console.ReadLine();
+        }
+    }
+}
